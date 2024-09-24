cf5afb1125c51bbaab10e541f21c272fc127de03
Leptonnw <kac4484@gmail.com>
2024-09-24 10:37:38 +0900

    9월24일 오전10시37분

--- src/main/java/com/example/demo/entity/BaseEntity.java
+++ src/main/java/com/example/demo/entity/BaseEntity.java
@@ -3,6 +3,7 @@ package com.example.demo.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
